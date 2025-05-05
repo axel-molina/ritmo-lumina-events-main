2daedcacc3b86d8be2c163c85dad52354245f06d
Axel Molina <ivanmolina052@gmail.com>
2025-05-05 02:17:12 -0300

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ import Marquee from "../components/Marquee";
 
 const Index = () => {
   // Set dark mode as default
-  console.log("Pr de prueba");
+  console.log("este es el console log que deberias avisarme");
 
   useEffect(() => {
     document.documentElement.classList.add("dark");
