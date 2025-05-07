769019a9d8ee0976046a988712043ab19880823e
Axel Molina <ivanmolina052@gmail.com>
2025-05-06 22:16:21 -0300

    testing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,8 +11,8 @@ import { useLocation } from "react-router-dom";
 
 const Index = () => {
   // Set dark mode as default
-  console.log("First render");
   const location = useLocation();
+  console.log("First");
 
   useEffect(() => {
     document.documentElement.classList.add("dark");
